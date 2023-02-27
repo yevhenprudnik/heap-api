ac59a588237956e30d760478d1d81ad9a768ec2a
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-02-27 23:57:03 +0200

    Add register query.

--- routes/auth/auth.schemas.js
+++ routes/auth/auth.schemas.js
@@ -1,8 +1,15 @@
-const user = {
+const logInUser = {
   email: {
     type: 'string',
   },
   password: {
+    type: 'string',
+  },
+};
+
+const registerUser = {
+  ...logInUser,
+  username: {
     type: 'string',
   },
 };
@@ -13,12 +20,29 @@ export const login = {
     body: {
       type: 'object',
       required: ['email', 'password'],
-      properties: user,
+      properties: logInUser,
     },
     response: {
       '2xx': {
         type: 'object',
-        properties: user,
+        properties: logInUser,
+      },
+    },
+  },
+};
+
+export const register = {
+  schema: {
+    description: 'Returns user or throws an error for already used credentials',
+    body: {
+      type: 'object',
+      required: ['email', 'password', 'username'],
+      properties: registerUser,
+    },
+    response: {
+      '2xx': {
+        type: 'object',
+        properties: registerUser,
       },
     },
   },
