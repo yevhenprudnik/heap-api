07c1b9501dd6686ad1fdd1243a3b25813ea6e48c
Kolesnik Oleksandr <okolesnik753@gmail.com>
2023-04-06 15:44:09 -0400

    Fix minor errors.

--- plugins/cookie.plugin.js
+++ plugins/cookie.plugin.js
@@ -1,17 +1,12 @@
 'use strict';
-
 import fp from 'fastify-plugin';
-
 import cookie from '@fastify/cookie';
-
 import { SECRET_KEY_COOKIE } from '../environment.js';
 
-export default fp((fastify, options, done) => {
+export default fp(async (fastify, options) => {
   fastify.register(cookie, {
     secret: SECRET_KEY_COOKIE,
   });
 
   console.log('Cookie plugin register');
-
-  done();
 });
