41fb05c30c784af17234f4a3fdf6bfb5ffdf1b79
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-05-04 23:55:09 +0300

    File naming fix

--- hooks/hooks.plugin.js
+++ hooks/hooks.plugin.js
@@ -1,19 +1,22 @@
 import fp from 'fastify-plugin';
 import { AuthHandlersService } from './auth-handlers.service.js';
-import { PostOwnershipChecker } from './post-handlers.service.js';
+import { PostOwnershipChecker } from './post-ownership-handler.service.js';
 
 export default fp(async fastify => {
   const authHandlersService = new AuthHandlersService(fastify.jwt);
   const postOwnershipChecker = new PostOwnershipChecker();
 
-  fastify.decorate('useAccessAuth', authHandlersService.useTokenAuth(['access']));
-
-  fastify.decorate('useRefreshAuth', authHandlersService.useTokenAuth(['refresh']));
+  fastify.decorate(
+    'useAccessAuth',
+    authHandlersService.useTokenAuth(['access'])
+  );
 
   fastify.decorate(
-    'usePostOwnership',
-    postOwnershipChecker.usePostOwnership
+    'useRefreshAuth',
+    authHandlersService.useTokenAuth(['refresh'])
   );
+
+  fastify.decorate('usePostOwnership', postOwnershipChecker.usePostOwnership);
 
   console.log('Hooks plugin registered.');
 });
