4650652a72e44b5ded103a3aa69a18c36077d479
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-05-08 17:02:12 +0300

    Clean-up test route.

--- routes/test/index.js
+++ routes/test/index.js
@@ -3,60 +3,24 @@ import { PostService } from '../../services/post.service.js';
 import { LikeService } from '../../services/like.service.js';
 import { CommentService } from '../../services/comment.service.js';
 
+const schema = {
+  schema: {
+    tags: ['Test'],
+    // ....
+  },
+};
+
 export default async fastify => {
   const userService = new UserService();
   const postService = new PostService();
-  const likeService = new LikeService();
   const commentService = new CommentService();
+  const likeService = new LikeService();
 
-  fastify.get(
-    '/user',
-    { schema: { tags: ['Test'] } },
-    async (request, reply) => {
-      return userService.search({}, []);
-    }
-  );
+  fastify.get('/', schema, async (request, reply) => {
+    console.log({
+      importedServices: [userService, postService, commentService, likeService],
+    });
 
-  fastify.get(
-    '/post',
-    { schema: { tags: ['Test'] } },
-    async (request, reply) => {
-      return postService.search({}, ['author', 'likes']);
-    }
-  );
-
-  fastify.get(
-    '/user-posts',
-    { schema: { tags: ['Test'] } },
-    async (request, reply) => {
-      // hardcode, just for testing:
-      const userId = 2;
-
-      return postService.search(
-        {
-          authorId: userId,
-        },
-        ['user', 'likes']
-      );
-    }
-  );
-
-  fastify.get(
-    '/like',
-    { schema: { tags: ['Test'] } },
-    async (request, reply) => {
-      return likeService.search({});
-    }
-  );
-
-  fastify.post(
-    '/like',
-    { preHandler: fastify.useAccessAuth, schema: { tags: ['Test'] } },
-    async (request, reply) => {
-      return likeService.create({
-        authorId: request.user.id,
-        postId: request.body.postId,
-      });
-    }
-  );
+    return { test: true };
+  });
 };
