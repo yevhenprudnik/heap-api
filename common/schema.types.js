f4c8bd147122c2581a746de9e9241ccde378134d
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-06-06 00:52:28 +0300

    Fix date schema.

--- common/schema.types.js
+++ common/schema.types.js
@@ -1,3 +1,7 @@
 export const typeString = { type: 'string' };
 export const typeNumber = { type: 'number' };
 export const typeBoolean = { type: 'boolean' };
+export const typeDate = {
+  type: 'string',
+  format: 'date',
+};
